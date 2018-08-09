a22e9a83caacd38775c5a6e537a8eb2de1f035c7
dgxwl <1029325214@qq.com>
2018-08-09 15:29:30 +0800

    add reflect field

---
+++ src/main/java/reflection/Demo3_callPrivateMethod.java
@@ -0,0 +1,26 @@
+package reflection;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+/**
+ * 利用反射机制访问私有方法
+ * @author JAVA
+ *
+ */
+public class Demo3_callPrivateMethod {
+	public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, NoSuchMethodException, SecurityException, IllegalArgumentException, InvocationTargetException {
+//		Person p = new Person();
+//		p.say();  //传统方式是无法再Person外部访问私有方法的
+		
+		Class<?> cls = Class.forName("reflection.Person");
+		Object obj = cls.newInstance();
+		Method method = cls.getDeclaredMethod("say", null);
+		/*
+		 * 强制设置允许访问该私有方法,访问私有方法前若不进行设置,name调用invoke()时会抛出权限不足的异常.
+		 * 但是需要注意,这样做违背了java面向对象中的封装性,实际开发中要谨慎使用.
+		 */
+		method.setAccessible(true);
+		method.invoke(obj, null);
+	}
+}
---
+++ src/main/java/reflection/Person.java
@@ -0,0 +1,32 @@
+package reflection;
+
+public class Person {
+	
+	public static final Integer HEIGHT = 170;
+	
+	private int weight = 60;
+	
+	public void sayHello() {
+		System.out.println("大家好!");
+	}
+	
+	public void sayHi() {
+		System.out.println("Hi!");
+	}
+	
+	public void sayBye() {
+		System.out.println("bye!");
+	}
+	
+	private void say() {
+		System.out.println("我是私有方法!");
+	}
+	
+	public void sayHello(String name) {
+		System.out.println("你好,"+name);
+	}
+	
+	public void sayHello(String name, int age) {
+		System.out.println("你好,"+name + ",您今年"+age+"岁");
+	}
+}
