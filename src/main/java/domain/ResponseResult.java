8fb34c1ec6be41dda7ad2443de93a93044c5f578
dgxwl <1029325214@qq.com>
2018-10-09 14:49:57 +0800

    modify rr

--- src/main/java/domain/ResponseResult.java
+++ src/main/java/domain/ResponseResult.java
@@ -1,9 +1,12 @@
 package domain;
 
+import java.util.Collection;
+import java.util.Map;
+
 public class ResponseResult {
 	private int result = 0;
 	private String message;
-	private int count;
+	private int total;
 	private Object data;
 	
 	public ResponseResult() {
@@ -34,12 +37,12 @@ public class ResponseResult {
 		this.message = message;
 	}
 
-	public int getCount() {
-		return count;
+	public int getTotal() {
+		return total;
 	}
 
-	public void setCount(int count) {
-		this.count = count;
+	public void setTotal(int total) {
+		this.total = total;
 	}
 
 	public Object getData() {
@@ -48,6 +51,13 @@ public class ResponseResult {
 
 	public void setData(Object data) {
 		this.data = data;
+		if (data instanceof Collection) {
+			Collection<?> c = (Collection<?>) data;
+			total = c.size();
+		} else if (data instanceof Map) {
+			Map<?, ?> m = (Map<?, ?>) data;
+			total = m.size();
+		}
 	}
 	
 	
