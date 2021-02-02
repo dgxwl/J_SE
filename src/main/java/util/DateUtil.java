879c9831fbb3af623c76b7b90b2ef23afbd80e91
dgxwl <1029325214@qq.com>
2021-02-02 17:44:09 +0800

    Update DateUtil.java
    优化

--- src/main/java/util/DateUtil.java
+++ src/main/java/util/DateUtil.java
@@ -9,90 +9,154 @@ public class DateUtil {
 	private DateUtil() {
 	}
 
-	public static Date getFirstDayOfMonth(Date date) {
+	public static Date getStartOfDay(Date date) {
 		if (date == null) {
-			throw new NullPointerException("Null Date");
-		}
-		Calendar cal = Calendar.getInstance();
-		cal.setTime(date);
-		cal.set(Calendar.DAY_OF_MONTH, 1);
-		cal.set(Calendar.HOUR_OF_DAY, 0);
-		cal.set(Calendar.MINUTE, 0);
-		cal.set(Calendar.SECOND, 0);
-		return cal.getTime();
-	}
-
-	public static Date getEndDayOfMonth(Date date) {
-		if (date == null) {
-			throw new NullPointerException("Null Date");
-		}
-		Calendar cal = Calendar.getInstance();
-		cal.setTime(date);
-		cal.add(Calendar.MONTH, 1);
-		cal.set(Calendar.DAY_OF_MONTH, 1);
-		cal.add(Calendar.DATE, -1);
-		cal.set(Calendar.HOUR_OF_DAY, 0);
-		cal.set(Calendar.MINUTE, 0);
-		cal.set(Calendar.SECOND, 0);
-		return cal.getTime();
-	}
-
-	public static Date getStartTimeOfDay(Date date) {
-		if (date == null) {
-			throw new NullPointerException("Null Date");
+			date = new Date();
 		}
 		Calendar cal = Calendar.getInstance();
 		cal.setTime(date);
 		cal.set(Calendar.HOUR_OF_DAY, 0);
 		cal.set(Calendar.MINUTE, 0);
 		cal.set(Calendar.SECOND, 0);
+		cal.set(Calendar.MILLISECOND, 0);
 		return cal.getTime();
 	}
 
-	public static Date getEndTimeOfDay(Date date) {
+	public static Date getEndOfDay(Date date) {
 		if (date == null) {
-			throw new NullPointerException("Null Date");
+			date = new Date();
 		}
 		Calendar cal = Calendar.getInstance();
 		cal.setTime(date);
 		cal.set(Calendar.HOUR_OF_DAY, 23);
 		cal.set(Calendar.MINUTE, 59);
 		cal.set(Calendar.SECOND, 59);
+		cal.set(Calendar.MILLISECOND, 999);
 		return cal.getTime();
 	}
 
-	public static String getFormattedCurrentDate() {
+	public static Date add(Date date, int field, int amount) {
+		if (date == null) {
+			date = new Date();
+		}
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(date);
+		cal.add(field, amount);
+		return cal.getTime();
+	}
+
+	public static Date addDays(Date date, int amount) {
+		if (date == null) {
+			date = new Date();
+		}
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(date);
+		cal.add(Calendar.DATE, amount);
+		return cal.getTime();
+	}
+
+	public static Date getStartOfDayAfterAdding(Date date, int amount) {
+		if (date == null) {
+			date = new Date();
+		}
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(date);
+		cal.add(Calendar.DATE, amount);
+		cal.set(Calendar.HOUR_OF_DAY, 0);
+		cal.set(Calendar.MINUTE, 0);
+		cal.set(Calendar.SECOND, 0);
+		cal.set(Calendar.MILLISECOND, 0);
+		return cal.getTime();
+	}
+
+	public static Date getEndOfDayAfterAdding(Date date, int amount) {
+		if (date == null) {
+			date = new Date();
+		}
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(date);
+		cal.add(Calendar.DATE, amount);
+		cal.set(Calendar.HOUR_OF_DAY, 23);
+		cal.set(Calendar.MINUTE, 59);
+		cal.set(Calendar.SECOND, 59);
+		cal.set(Calendar.MILLISECOND, 999);
+		return cal.getTime();
+	}
+
+	public static Date getStartOfDayAfterAdding(Date date, int field, int amount) {
+		if (date == null) {
+			date = new Date();
+		}
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(date);
+		cal.add(field, amount);
+		cal.set(Calendar.HOUR_OF_DAY, 0);
+		cal.set(Calendar.MINUTE, 0);
+		cal.set(Calendar.SECOND, 0);
+		cal.set(Calendar.MILLISECOND, 0);
+		return cal.getTime();
+	}
+
+	public static Date getEndOfDayAfterAdding(Date date, int field, int amount) {
+		if (date == null) {
+			date = new Date();
+		}
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(date);
+		cal.add(field, amount);
+		cal.set(Calendar.HOUR_OF_DAY, 23);
+		cal.set(Calendar.MINUTE, 59);
+		cal.set(Calendar.SECOND, 59);
+		cal.set(Calendar.MILLISECOND, 999);
+		return cal.getTime();
+	}
+
+	public static Date getStartOfMonth(Date date) {
+		if (date == null) {
+			date = new Date();
+		}
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(date);
+		cal.set(Calendar.DAY_OF_MONTH, 1);
+		cal.set(Calendar.HOUR_OF_DAY, 0);
+		cal.set(Calendar.MINUTE, 0);
+		cal.set(Calendar.SECOND, 0);
+		cal.set(Calendar.MILLISECOND, 0);
+		return cal.getTime();
+	}
+
+	public static Date getEndOfMonth(Date date) {
+		if (date == null) {
+			date = new Date();
+		}
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(date);
+		cal.set(Calendar.DATE, cal.getMaximum(Calendar.DATE));
+		cal.set(Calendar.HOUR_OF_DAY, 23);
+		cal.set(Calendar.MINUTE, 59);
+		cal.set(Calendar.SECOND, 59);
+		cal.set(Calendar.MILLISECOND, 999);
+		return cal.getTime();
+	}
+
+	public static String getFormattedDate(Date date) {
+		if (date == null) {
+			date = new Date();
+		}
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		Date now = new Date();
-		return sdf.format(now);
-	}
-
-	public static String getFormattedCurrentDate(String format) {
-		SimpleDateFormat sdf = new SimpleDateFormat(format);
-		Date now = new Date();
-		return sdf.format(now);
-	}
-
-	public static Date getYesterday() {
-		Calendar cal = Calendar.getInstance();
-		cal.add(Calendar.DATE, -1);
-		return cal.getTime();
-	}
-
-	public static String getYesterday(String format) {
-		SimpleDateFormat sdf = new SimpleDateFormat(format);
-		Calendar cal = Calendar.getInstance();
-		cal.add(Calendar.DATE, -1);
-		return sdf.format(cal.getTime());
+		return sdf.format(date);
 	}
 
 	public static String getFormattedDate(Date date, String format) {
+		if (date == null) {
+			date = new Date();
+		}
 		SimpleDateFormat sdf = new SimpleDateFormat(format);
 		return sdf.format(date);
 	}
 
 	public static Date parseDate(String date) throws ParseException {
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		return sdf.parse(date);
 	}
 
@@ -104,7 +168,7 @@ public class DateUtil {
 	/**
 	 * 求两个日期相差天数
 	 * @param date1 第一个日期,小的
-	 * @param date2第二个日期,大的
+	 * @param date2 第二个日期,大的
 	 * @return 相差天数
 	 */
 	public int differentDays(Date date1, Date date2) {
